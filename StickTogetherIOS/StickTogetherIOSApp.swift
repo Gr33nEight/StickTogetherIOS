58f5d94104e3a29a8b345d76c9f36b9ae6f4ac02
Gr33nEight <natanael.jop.app@gmail.com>
2025-10-29 12:05:31 +0100

    initial commit

---
+++ StickTogetherIOS/StickTogetherIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickTogetherIOSApp.swift
+//  StickTogetherIOS
+//
+//  Created by Natanael Jop on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct StickTogetherIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
