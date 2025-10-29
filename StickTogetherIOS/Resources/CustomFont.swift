a1a75dbdbc778f5f867aedc2a9bd1caa68a284c9
Natanael <93977274+Gr33nEight@users.noreply.github.com>
2025-10-29 15:03:12 +0100

    Basic styles applied

---
+++ StickTogetherIOS/Resources/CustomFont.swift
@@ -0,0 +1,50 @@
+//
+//  Font+App.swift
+//  StickTogetherIOS
+//
+//  Created by Natanael Jop on 29/10/2025.
+//
+
+import SwiftUI
+
+enum CustomFont {
+    case heading
+    case subheading
+    case body
+    case caption
+    case custom(size: CGFloat, weight: Font.Weight = .regular)
+    
+    private var family: String { "HelveticaNeue" }
+    
+    var font: Font {
+        switch self {
+        case .heading:
+            return Font.custom("\(family)-Heavy", size: 32, relativeTo: .largeTitle)
+        case .subheading:
+            return Font.custom("\(family)-Bold", size: 24, relativeTo: .title)
+        case .body:
+            return Font.custom("\(family)-Medium", size: 18, relativeTo: .body)
+        case .caption:
+            return Font.custom("\(family)-Light", size: 14, relativeTo: .caption)
+        case .custom(let size, let weight):
+            return Font.custom("\(family)-\(weight.fontNameSuffix)", size: size)
+        }
+    }
+}
+
+private extension Font.Weight {
+    var fontNameSuffix: String {
+        switch self {
+        case .ultraLight: return "UltraLight"
+        case .thin: return "Thin"
+        case .light: return "Light"
+        case .regular: return "Regular"
+        case .medium: return "Medium"
+        case .semibold: return "Semibold"
+        case .bold: return "Bold"
+        case .heavy: return "Heavy"
+        case .black: return "Black"
+        default: return "Regular"
+        }
+    }
+}
